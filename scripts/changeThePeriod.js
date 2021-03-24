11374b22a16a11de7c30a6f99d8b4d3f82ba466b
unknown <w2095924601@qq.com>
2021-03-24 17:41:12 +0800

    add some js/update three web

---
+++ scripts/changeThePeriod.js
@@ -0,0 +1,4 @@
+var lunchBreak=document.getElementById('lunchBreak');
+const habits=document.getElementById('habits');
+const lunchBreakHabits = document.getElementsByClassName('lunchBreakHabits')
+console.log(lunchBreakHabits)
